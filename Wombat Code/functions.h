a86ed2b58529cf73ae54463b5d421cb81974ccdc
botball750 <102451952+botball750@users.noreply.github.com>
2024-03-27 16:23:26 -0700

    Add files via upload

---
+++ Wombat Code/functions.h
@@ -0,0 +1,48 @@
+// Variables
+int Servo_Arm = 0;
+int Servo_Claw = 1;
+
+// Initialization
+void init()
+{
+    create3_connect();
+    printf("Starting Create3 Code!\n");
+}
+
+// Perform at end of each function
+void End_Of_Code()
+{
+    create3_wait();
+    printf("Process Executed Successfully.\n");
+}
+
+// Drive Distance in Meters
+void driveM(int distance, int speed)
+{
+    create3_drive_straight(distance, speed);
+    End_Of_Code();
+}
+
+// Drive Distance in Feet
+void driveFT(int distance, int speed)
+{
+    create3_drive_straight(distance * 0.333, speed);
+    End_Of_Code();
+}
+
+// Rotate Create3 in any degree
+void rotate(int rotation, int speed)
+{
+    create3_rotate_degrees(rotation, speed);
+    End_Of_Code();
+}
+
+// Move any servo in the arguemnts.
+void moveServo(int port, int position)
+{
+    enable_servos();
+    set_servo_position(port, position);
+
+    End_Of_Code();
+    disable_servos();
+}
---
+++ Wombat Code/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <kipr/wombat.h>
+#include <functions.h>
+
+// Execute Code
+int main()
+{
+    init();
+    
+    rotate(50000,90);
+    
+    create3_wait();
+    return 0;
+}
