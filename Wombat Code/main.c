cad8b4808607a608ebb20709f4c63519f1acec43
botball750 <102451952+botball750@users.noreply.github.com>
2024-03-29 22:51:25 -0700

    Updated the Code to be more changable
    Make it easier for customization and have better and shorter functions.

--- Wombat Code/main.c
+++ Wombat Code/main.c
@@ -1,10 +1,26 @@
+// Import Libraries
 #include <stdio.h>
 #include <kipr/wombat.h>
 #include <functions.h>
 
-// Execute Code
+// Constant / Ports
+const int motor_one = 0;
+const int motor_two = 1;
+
+const int sensor_one = 0;
+const int sensor_two = 1;
+
+const int servo_one = 0;
+const int servo_two = 0;
+
 int main()
 {
+  // Reference Guide
+  /*
+    mavs(speed (percentage) - Utilize both motors
+    drive(distance (ticks) , speed (percentage) - Drive while using GMPC
+  */
 
-    return 0;
+
+  return 0;
 }
