60af9e95b93fe776950135ba7e4973986661c5a6
botball750 <102451952+botball750@users.noreply.github.com>
2024-04-12 08:37:11 -0700

    Updated the code to get the rocks instead

--- Roomba Code/main.c
+++ Roomba Code/main.c
@@ -1,51 +1,30 @@
 #include <stdio.h>
 #include <kipr/wombat.h>
-#include <functions.h>
+#include <main.h>
+
+// Variables
+int Servo_Arm = 0;
+int Servo_Claw = 1;
 
 // Execute Code
 int main()
 {
-    init(); // Start
-	// Adjust Servos
-    moveServo(0, 225);
-    moveServo(1, 444);
-
-    msleep(1000);
-	// Drive Towards Pom 1
-    driveFT(.7, 1);
-
-    rotate(30, 90); // Knock Pom 1
-    msleep(500);
+    init();
+	
+    moveServo(Servo_Claw, 1700);
+    moveServo(Servo_Arm, 200);
     
-    rotate(-40, 90);
-    driveFT(0.5,1);
+    rotate(10,5);
+    driveFT(4,5);
+    moveServo(Servo_Claw, 600);
     
-    rotate(40, 90);
-    rotate(-40, 90);
-    moveServo(1, 2047);
-    // Finish Knocking Pom Two
+    rotate(25,5);
+    moveServo(Servo_Claw, 1700);
+    driveFT(2, 1);
     
-    // Start Driving to middle
-    driveFT(2,1);
-    rotate(60, 90);
-    driveFT(1.3,1);
-    driveFT(-1.65, 1);
-    rotate(25, 40);
-    
-    // Adjust to the height of the cubes
-    moveServo(0,1700);
-    driveFT(1.05,1);
-    moveServo(1, 350); // Grab the Cube
-    msleep(1000);
-    driveFT(-0.5,1);
-    rotate(30, 90);
-    
-    // Drop off the Cubes to the left
-    driveFT(0.1,1);
-    moveServo(0,225);
-    msleep(500);
-    moveServo(1, 2047);
-
-    create3_wait(); // End
+    driveFT(-1, 1);
+    rotate(-90,45);
+   
+    create3_wait();
     return 0;
 }
