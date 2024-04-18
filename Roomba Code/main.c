8f8cf35433768dbae8562be9352183b465446375
botball750 <102451952+botball750@users.noreply.github.com>
2024-04-18 11:10:56 -0700

    New Code + 4 Rocks in Rock Heap

--- Roomba Code/main.c
+++ Roomba Code/main.c
@@ -5,26 +5,61 @@ int main()
 {
     //wait_for_light(1);
     shut_down_in(115);
-    
+
     create_connect();
     enable_servos();
-    
-    movegmpc(700,200, 200);
+
+
+    // Setup
+    printf("hello world\n");
+    create_drive_direct(-200,-200); // Align with back wall
+    set_servo_position(0, 1000);
+    sleep(1);
+    set_servo_position(1,750);
+    sleep(1);
+    set_servo_position(2,2047);
+    sleep(1);
+
+    sleep(3);
+
+    movegmpc(700, 200, 200); // Go to Pile 1
     ao();
-    
-    turn(30); // Turn to align to two rocks
-    
+
+    turn(45);
+
+    set_servo_position(1, 100); // Claw Down
     sleep(1);
-    set_servo_position(2, 1300); // Push rocks together
+    set_servo_position(2, 1500); // Claw Open Equal
     sleep(1);
+
+    movegmpc(150, 50, 50); // Go up Slowly
+    servo(2, 200, 1); // Bring Rocks Together
+
+    sleep(1);
+    turn(-30);
+
+    set_servo_position(2, 1500); // Open Claw
+    movegmpc(150, 50, 50); // Go to Alone Rock
+    sleep(0.5);
+    servo(2, 0, 1); // Grab Third Rock
+
+    sleep(1);
+    turn(45); // Face Towards zone + last rock
+    movegmpc(150, 100, 100);
+    set_servo_position(2, 1000);
+    movegmpc(650, 200, 200); 
     
-    movegmpc(200, 200, 200); // Move up a little to fully grab the two rocks
-    set_servo_position(2, 300); // Fully Close
+    create_drive_direct(-100, -100);
+    sleep(0.5);
+    turn(10);
+    create_drive_direct(-200, -200);
+    sleep(3);
     
-    movegmpc(700, 200, 200); // Go Towards 3rd rock
-    set_servo_position( 2, 1800); // Open Claw to get the third rock
+    turn(15);
+    create_drive_direct(-200, -200);
+    sleep(2.5);
     
-    movegmpc(500, 200, 200); // Deliver Rocks
-    
+    set_servo_position(2, 1700);
+    movegmpc(300, 150, 150);
     return 0;
 }
